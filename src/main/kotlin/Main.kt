300e069b7514fb6f12ebc652b7b6c12e754baf90
Evgeniy Cherkasov <evgenii.cherkasov@yandex.ru>
2023-10-25 17:27:47 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,72 @@
+fun main() {
+    val card = "VK Pay"
+    val transfer = 10000
+    val sumTransfer = 30000
+    println("Сумма перевода - $transfer")
+    if (transfer > 150000) {
+        println("Сумма больше лимита 150000")
+    } else {
+        when (card) {
+            "Master card", "Maestro" -> {
+                if (masterCard(transfer, sumTransfer) != -1.0) {
+                    println("Комиссия: ${masterCard(transfer, sumTransfer)}")
+                    println("Сумма перевода: ${transfer - masterCard(transfer, sumTransfer)}")
+                } else {
+                    println("Перевод невозможен")
+                }
+            }
+
+            "Visa", "Мир" -> {
+                if (visa(transfer, sumTransfer) != -1.0) {
+                    println("Комиссия: ${visa(transfer, sumTransfer)}")
+                    println("Сумма перевода: ${transfer - visa(transfer, sumTransfer)}")
+                } else {
+                    println("Перевод невозможен")
+
+                }
+            }
+
+            "VK Pay" -> {
+                if (vk(transfer, sumTransfer)) {
+                    println("Комиссия: 0")
+                    println("Сумма перевода: $transfer")
+                } else {
+                    println("Перевод невозможен")
+                }
+            }
+        }
+
+    }
+}
+
+
+fun masterCard(transfer: Int, sumTransfer: Int): Double = when (sumTransfer) {
+    in 0..75_000 -> 0.0
+    in 75_000..600_000 -> transfer / 100 * 0.6 + 20
+    else -> -1.0
+}
+
+fun visa(transfer: Int, sumTransfer: Int): Double {
+    if (sumTransfer > 600000) {
+        return -1.0
+    }
+    val result: Double = transfer / 100 * 0.75
+    if (transfer > 35.0) {
+        if (result > 35.0) {
+            return result
+        } else {
+            return 35.0
+        }
+
+    }
+    return -1.0
+}
+
+fun vk(transfer: Int, sumTransfer: Int): Boolean {
+    if (transfer < 15000) {
+        if (sumTransfer < 40000) {
+            return true
+        }
+    }
+    return false
+}
